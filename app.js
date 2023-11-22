b1850d254e7252c0db4f7416a93200ce499dadad
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-22 20:20:51 +0530

    created the show route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // Connection with the database
 const mongo_url = "mongodb://127.0.0.1:27017/roomvista";
@@ -28,6 +29,13 @@ app.get("/", (req, res) => {
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+
+//Show route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // app.get("/testListing", async (req, res) => {
