25a3e4d19f21b158cfafa9a046c9190f818f7409
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-22 14:41:01 +0530

    created an index route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Connection with the database
 const mongo_url = "mongodb://127.0.0.1:27017/roomvista";
@@ -20,19 +24,25 @@ app.get("/", (req, res) => {
   res.send("hi I am root");
 });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "my new villa",
-    description: "by the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
-
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("successful testing");
+// Index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "my new villa",
+//     description: "by the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
 // Connection with the server
 app.listen(8080, () => {
