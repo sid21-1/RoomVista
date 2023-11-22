3f21ce1ac64e60fd1bf846e9209167c4a7c719dc
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-11-22 10:35:52 +0530

    created the listing model

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
+// Connection with the database
 const mongo_url = "mongodb://127.0.0.1:27017/roomvista";
 main()
   .then(() => {
@@ -13,10 +15,26 @@ async function main() {
   await mongoose.connect(mongo_url);
 }
 
+// API's
 app.get("/", (req, res) => {
   res.send("hi I am root");
 });
 
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "my new villa",
+    description: "by the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("successful testing");
+});
+
+// Connection with the server
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
 });
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  images: {
+    default:
+      "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    type: String,
+    set: (v) =>
+      v === ""
+        ? "https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
