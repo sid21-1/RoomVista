bf612174dca3d613bbf68cd48e8c4b32b93669ff
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-12-14 11:08:04 +0530

    implemented sessions in the project

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 // const { error } = require("console");
+const session = require("express-session");
 
 const { log } = require("console");
 
@@ -18,6 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
 
 // Connection with the database
 const mongo_url = "mongodb://127.0.0.1:27017/roomvista";
